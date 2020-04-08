4f1a6948dd06e9273e52f3a5969c4541e0a413c5
ohmyguigs <ohmyguigs@gmail.com>
2020-04-08 15:29:04 -0300

    WIP - trying to join zoom room

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ console.log('checkSystemRequirements');
 console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 
 // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
+// ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.2/lib', '/av'); // CDN version default
 // if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.2/lib', '/av'); // CDN version default
 // else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.2/lib', '/av'); // china cdn option 
-// ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
+ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
 
-
-const API_KEY = 'YOUR_API_KEY';
+const API_KEY = "";
 
 /**
     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
     * The below generateSignature should be done server side as not to expose your api secret in public
     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
     */
-const API_SECRET = 'YOUR_API_SECRET';
+const API_SECRET = "";
 
 document.getElementById('join_meeting').addEventListener('click', (e) => {
     e.preventDefault();
@@ -27,7 +27,8 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
         apiKey: API_KEY,
         apiSecret: API_SECRET,
         meetingNumber: parseInt(document.getElementById('meeting_number').value, 10),
-        userName: document.getElementById('display_name').value,
+        // userName: document.getElementById('display_name').value,
+        userName: 'Gandhi',
         passWord: '',
         leaveUrl: 'https://zoom.us',
         role: 0
@@ -39,7 +40,7 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
         apiSecret: meetConfig.apiSecret,
         role: meetConfig.role,
         success(res) {
-            console.log('signature', res.result);
+            console.log('success generating signature - ', res.result);
             ZoomMtg.init({
                 leaveUrl: 'http://www.zoom.us',
                 success() {
@@ -49,7 +50,7 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
                             userName: meetConfig.userName,
                             signature: res.result,
                             apiKey: meetConfig.apiKey,
-                            userEmail: 'email@gmail.com',
+                            userEmail: 'ohmyguigs@gmail.com',
                             passWord: meetConfig.passWord,
                             success() {
                                 $('#nav-tool').hide();
